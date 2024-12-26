f26991c69d301f1745616072fd7d6bf8c759157c
tamartre <tmrk67419@gmail.com>
2024-12-26 23:29:37 +0200

    some changes

--- my-app/src/app/student-list/student-list.component.ts
+++ my-app/src/app/student-list/student-list.component.ts
@@ -11,7 +11,7 @@ export class StudentListComponent {
   students:Student[]=[
     {id:1,firstName:"Tamar",lastName:"Treger",address:"26 Henry",phone:556772499,isActive:false,marksAverage:100},
     {id:2,firstName:"Shifra",lastName:"Choen",address:"2 Hagefen",phone:527123425,isActive:true,marksAverage:87},
-    {id:3,firstName:"Noa",lastName:"Levi",address:"",phone:"12 Sderot",isActive:true,marksAverage:92}
+    {id:3,firstName:"Noa",lastName:"Levi",address:"12 Sderot",phone:527173297,isActive:true,marksAverage:92}
   ]
 
   DeleteStudent(id: number) {
